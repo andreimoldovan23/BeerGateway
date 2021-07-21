064f950a78e4a3884f1eb4eb7656fb02e8a35fe5
andreimoldovan23 <moldovanandrei2301@gmail.com>
2021-07-21 14:58:46 +0300

    Configured routing for load balancer

--- src/main/java/sfmc/gateway/config/LoadBalancedRouteConfig.java
+++ src/main/java/sfmc/gateway/config/LoadBalancedRouteConfig.java
@@ -6,18 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!localdiscovery")
+@Profile("localdiscovery")
 @Configuration
-public class LocalHostRouteConfig {
+public class LoadBalancedRouteConfig {
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("beer-service", r -> r.path("/api/v1/beer/*", "/api/v1/beerUpc/*")
-                    .uri("http://localhost:8080"))
+                        .uri("lb://beer-service"))
                 .route("beer-order-service", r -> r.path("/api/v1/customers/**")
-                    .uri("http://localhost:8083"))
+                        .uri("lb://beer-order-service"))
                 .route("beer-inventory-service", r -> r.path("/api/v1/beer/*/inventory")
-                    .uri("http://localhost:8082"))
+                        .uri("lb://beer-inventory-service"))
                 .build();
     }
 }
